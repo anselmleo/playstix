33047d1a8b5287419065e13c791dc30ef48d162e
Anselm <anselmleo@gmail.com>
2021-01-18 21:30:16 +0100

    edit index.css

--- src/components/DPNav/DPNav.js
+++ src/components/DPNav/DPNav.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Styles from './DPNav.module.css';
 import dp from '../../assets/images/dp.svg';
 import dollar from '../../assets/images/dollar.svg';
+import networkSVG from '../../assets/images/network.svg';
 
 function DPNav() {
     return (
         <div className={Styles.nav}>
-            <div>
+            <div className={Styles.dpNavLeft}>
                 <img src={dp} className="" alt="dp" />
                 <img src={dollar} className="" alt="dollar" />
                 <p>Agent Playstix</p>
+            </div>
+            <div className={Styles.dpNavRight}>
+                <img src={networkSVG} className="" alt="networkSVG" />
             </div>
         </div>
     )
