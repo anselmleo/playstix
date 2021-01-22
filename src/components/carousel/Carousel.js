3536e541b620d4dc5016b2c3eb9f0a27451f4868
unknown <falola.adedayo@thegiggroupng.com>
2021-01-22 22:49:31 +0100

    background images added

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,73 +1,41 @@
-import React, { useEffect, useState } from "react";
 import { Col, Row, Card, Carousel } from "react-bootstrap";
 import Style from "./Carousel.module.css";
-import movieList from "../../services/movieAPI/movieAPIService";
 
-function MovieCarousel() {
-  const [actionMovies, setActionMovies] = useState([]);
-  const [movieImage, setMovieImage] = useState("");
-  const getActionMovieList = (e) => {
-    movieList
-      .getAllMovies()
-      .then((response) => {
-        console.log("response", response);
-        setActionMovies(response.data.results);
-      })
-      .catch((e) => {
-        console.log("the error", e.response);
-      });
-  };
-
-  useEffect(() => {
-    getActionMovieList();
-  }, []);
-
-  console.log("got response", actionMovies);
-
-  // movieList.getAllMovieImages(actionMovie.backdrop_path)
-  // .then((response) => {
-
-  //  console.log(response)
-  // })
-  // .catch((e) => {
-  //   console.log("ddddd", e.response);
-  // })
+function MovieCarousel({ actionMovies }) {
+  let movieSplit = [];
+  let imageCards = [];
+  actionMovies.forEach((card) => {
+    movieSplit.push(card);
+    if (movieSplit.length === 8) {
+      imageCards.push(movieSplit);
+      movieSplit = [];
+    }
+  });
+  if (movieSplit.length) {
+    imageCards.push(movieSplit);
+  }
 
   return (
     <div>
-      <Carousel indicators={false} controls>
-        <Carousel.Item interval={1000}>
-          <div className={Style.cardcontainer}>
-            <Row>
-              {actionMovies.map((actionMovie) => (
-                <Col className="mt-1 p-0" sm={3}>
-                  <Card className={Style.card}>
-                    <Card.Img
-                      className={Style.image}
-                      src={`https://image.tmdb.org/t/p/w300/${actionMovie.backdrop_path}`}
-                    />
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </div>
-        </Carousel.Item>
-        <Carousel.Item>
-          <div className={Style.cardcontainer}>
-            <Row>
-              {actionMovies.map((actionMovie) => (
-                <Col className="mt-1 p-0" sm={3}>
-                  <Card className={Style.card}>
-                    <Card.Img
-                      className={Style.image}
-                      src={`https://image.tmdb.rg/t/p/w300/${actionMovie.backdrop_path}`}
-                    />
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </div>
-        </Carousel.Item>
+      <Carousel indicators={false} controls={true}>
+        {imageCards.map((imageCard, index) => (
+          <Carousel.Item key={index}>
+            <div className={Style.cardcontainer}>
+              <Row>
+                {imageCard.map((card) => (
+                  <Col key={card.id} className="mt-1 p-0" sm={3}>
+                    <Card className={Style.card}>
+                      <Card.Img
+                        className={Style.image}
+                        src={`https://image.tmdb.org/t/p/w300/${card.backdrop_path}`}
+                      />
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </div>
+          </Carousel.Item>
+        ))}
       </Carousel>
     </div>
   );
